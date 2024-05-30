6648e1222ebe98665e9ad33fe6415710147caa6a
erichiroshi <erichiroshi@gmail.com>
2024-05-29 20:28:47 -0400

    Task 1 - Done

--- myinterview/src/main/java/com/example/TASK1.java
+++ myinterview/src/main/java/com/example/TASK1.java
@@ -1,5 +1,7 @@
 package com.example;
+
 import java.util.Scanner;
+
 /**
  * 
  *
@@ -13,6 +15,48 @@ import java.util.Scanner;
  * racecar.
  */
 public class TASK1 {
-     
- 
+
+	/**
+	 * 
+	 * Função que verifica se uma String é ou não palídromo
+	 * 
+	 * @param s String para verificação
+	 * @return true se for palíndromo
+	 */
+	public static boolean isPalindrome(String s) {
+
+		// Remove os espaços e converte tudo para maiúsculos
+		s = s.replaceAll("\\s+", "").toUpperCase();
+
+		// Início das comparações da string
+		int esquerda = 0;
+		int direita = s.length() - 1;
+
+		// Vai comparando até chegar no meio da string
+		while (esquerda < direita) {
+			if (s.charAt(esquerda) != s.charAt(direita)) {
+				return false;
+			}
+			esquerda++;
+			direita--;
+		}
+		return true;
+	}
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		// Lê a string
+		String palavra = sc.nextLine();
+
+		// verifica se é palíndromo
+		if (isPalindrome(palavra)) {
+			System.out.println("É Palíndromo");
+		} else {
+			System.out.println("Não é Palíndromo");
+		}
+
+		sc.close();
+	}
 }
