3725d036b2da7f118691f5d2b2ca9ab9032065fa
erichiroshi <erichiroshi@gmail.com>
2024-05-31 11:26:09 -0400

    Task 3 - Done

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -1,7 +1,11 @@
 package com.example;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import java.util.Set;
 
 /**
  * Write a list and add an aleatory number of Strings. In the end, print out how
@@ -10,5 +14,36 @@ import java.util.Iterator;
  */
 public class TASK3 {
 
-  
+	public static void main(String[] args) {
+
+		// lista para armazenar as strings geradas
+		List<String> stringList = new ArrayList<String>();
+
+		// gera um número aleatório, para o tamanho da lista
+		Random random = new Random();
+		int numberOfStrings = random.nextInt(20) + 1; // de 1 - 20 strings
+
+		for (int i = 0; i < numberOfStrings; i++) {
+			// gerando strings aleatórias, no formato: "random0 - random9"
+			String str = "random" + random.nextInt(10);
+			stringList.add(str);
+		}
+
+		// imprime lista completa
+		System.out.println("Lista de strings: " + stringList);
+
+		// conjunto (Set) para Strings distintas
+		Set<String> distinctItems = new HashSet<String>();
+
+		// Iterator para percorrer a lista e adicionar no conjunto
+		Iterator<String> iterator = stringList.iterator();
+		while (iterator.hasNext()) {
+			String item = iterator.next();
+			distinctItems.add(item);
+		}
+
+		// Imprime a quantidade de itens distintos
+		System.out.println("Número de itens distintos: " + distinctItems.size());
+	}
+
 }
